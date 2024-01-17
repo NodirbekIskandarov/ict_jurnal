8b176305c7b7293d0482bcfc559ffc89bb3c8073
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-01-17 23:04:33 +0500

    new changes

---
+++ src/components/Brands/Brands.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Brands() {
+  return (
+    <div className='brands'>
+        
+    </div>
+  )
+}
+
+export default Brands
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,28 +1,42 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-export default class SimpleSlider extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <div>
-        <h2>Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-        </Slider>
-      </div>
-    );
-  }
+import './slider.scss'
+import img from '../../assets/images/slider.png'
+import blur from '../../assets/images/blur.png'
+import { Link } from "react-router-dom";
+function Sliders() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="slider">
+      <Slider {...settings}>
+        <div className="slider_item">
+            <div className="blur">
+                <h3>A little text about the collection of the brightest products</h3>
+                <div className="description_slide">
+                    <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    <Link to='/' className="link">Maqolalarni ko’rmoq</Link>
+                </div>
+            </div>
+        </div>
+        <div className="slider_item">
+            <div className="blur">
+                <h3>A little text about the collection of the brightest products</h3>
+                <div className="description_slide">
+                    <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    <Link to='/' className="link">Maqolalarni ko’rmoq</Link>
+                </div>
+            </div>
+        </div>
+      </Slider>
+    </div>
+  );
 }
+export default Sliders;
